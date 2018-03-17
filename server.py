df7cb3f8f5c4fdf1441f44743bbacac3e72b5591
Alejandro <ale@Alejandros-MacBook-Pro.local>
2018-03-17 23:32:05 +0100

    Added some preprocesing to the sentences

--- server.py
+++ server.py
@@ -2,8 +2,8 @@ from model.data_utils import CoNLLDataset
 from model.ner_model import NERModel
 from model.config import Config
 from flask import Flask, request
-from num2words import num2words
 import json
+import server_utils
 
 app = Flask(__name__)
 config = Config()
@@ -14,60 +14,24 @@ model.restore_session(config.dir_model)
 def main():
     app.run(debug=True)
 
-# def digits2words(sentence):
-#     return
-#
-# def get_digits1(text):
-#     char = ""
-#     for i in range(len(text)):
-#         if text[i].isdigit():
-#             number = text[i]
-#
-#     return c
-
 
 @app.route('/parse', methods=['POST'])
 def parse():
     data = request.get_data()
-    sentence = data.strip().decode().split(" ")
-    # TODO preprosessing, typo-fixing, lowercase, digits to words
-    prediction = model.predict(sentence)
+    sentence = data.strip().decode()
+    preprocessed_sentence = server_utils.preprocess(sentence)
+    preprocessed_sentence = preprocessed_sentence.split(' ')
+    sentence = sentence.split(' ')
 
-    parsed = {
-        'type': '',
-        'departure': '',
-        'destination': '',
-        'departureDate': '',
-        'returnDate': ''
-    }
+    prediction = model.predict(preprocessed_sentence)
+    parsed = server_utils.parseLabels(sentence, prediction)
 
-    for i in range(len(prediction)):
-        label = prediction[i]
-        word = sentence[i]
+    # get iatas from cities
+    parsed['departure'] = server_utils.getIATA(parsed['departure'])
+    parsed['destination'] = server_utils.getIATA(parsed['destination'])
 
-        if label == "B-round_trip":
-            parsed['type'] = word
-        elif label == "I-round_trip":
-            parsed['type'] += ' ' + word
-        elif label == "B-toloc.city_name":
-            parsed['destination'] = word
-        elif label == "I-toloc.city_name":
-            parsed['destination'] += ' ' + word
-        elif label == "B-fromloc.city_name":
-            parsed['departure'] = word
-        elif label == "I-fromloc.city_name":
-            parsed['departure'] += ' ' + word
-        elif label.startswith("B-depart_date"):
-            parsed['departureDate'] = word
-        elif label.startswith("I-depart_date"):
-            parsed['departureDate'] += ' ' + word
-        elif label.startswith("B-return_date"):
-            parsed['returnDate'] = word
-        elif label.startswith("I-return_date"):
-            parsed['returnDate'] += ' ' + word
-
-    return json.dumps(prediction) + '\n' + json.dumps(parsed) + '\n'
-
+    # todo parse: times, dates, return trips
+    return json.dumps(prediction) + '\n' +json.dumps(parsed) + '\n'
 
 if __name__ == '__main__':
    main()
---
+++ server_utils.py
@@ -0,0 +1,99 @@
+import http.client
+import json
+import csv
+import re
+
+def preprocess(sentence):
+    encoded = encodeDigits(sentence)
+    encoded = encodeMonths(encoded)
+    encoded = encodeIatas(encoded)
+    return encoded
+
+
+def getIATA(city):
+    conn = http.client.HTTPSConnection("www.google.es")
+    request = {
+      '2': city
+    }
+    payload = json.dumps({
+      '1':[
+        {
+          '1': 'aa',
+          '2': json.dumps(request)
+        }
+      ]
+    })
+    headers = {
+        'x-gwt-permutation': "C6AE2226F2736ED890C050AF7708C2FD",
+        'cache-control': "no-cache",
+    }
+    conn.request("POST", "/flights/rpc", payload, headers)
+    res = conn.getresponse()
+    data = json.loads(res.read().decode("utf-8"))
+    aiports = json.loads(data['1'][0]['2'])
+    try:
+        return aiports['3'][0]['2']
+    except KeyError:
+        return ''
+
+def validateIATA(iata):
+    code = iata.upper().strip()
+    with open('data/IATAs.csv', 'rt') as f:
+        reader = csv.reader(f, delimiter=',')
+        for row in reader:
+            if code == row[0]:
+                return True
+    return False
+
+def encodeDigits(sentence):
+    return ''.join(list(map(lambda x: 'DIGIT' if x.isdigit() else x, sentence)))
+
+def encodeMonths(sentence):
+    return re.sub(r'(?i)(january|february|march|april|may|june|july|august|september|october|november|december)', 'MONTH', sentence)
+
+def encodeIatas(sentence):
+    candidates = re.findall(r'\b[a-zA-Z]{3}\b', sentence)
+    iatas = list(filter(validateIATA, candidates))
+    regexExp = re.compile('\\b(' + '|'.join(iatas) + ')\\b')
+    return re.sub(regexExp, 'IATA', sentence)
+
+def parseLabels(sentence, prediction):
+    parsed = {
+        'type': '',
+        'departure': '',
+        'destination': '',
+        'departureDate': '',
+        'departureTime': '',
+        'returnDate': ''
+    }
+
+    for i in range(len(prediction)):
+        label = prediction[i]
+        word = sentence[i]
+
+        if label == "B-round_trip" or label == "I-round_trip":
+            parsed['type'] = 'round_trip'
+        elif label == "B-ow_trip" or label == "I-ow_trip":
+            parsed['type'] = 'ow_trip'
+        elif label == "B-toloc.city_name":
+            parsed['destination'] = word
+        elif label == "I-toloc.city_name":
+            parsed['destination'] += ' ' + word
+        elif label == "B-fromloc.city_name":
+            parsed['departure'] = word
+        elif label == "I-fromloc.city_name":
+            parsed['departure'] += ' ' + word
+        elif label.startswith("B-depart_date"):
+            parsed['departureDate'] = word
+        elif label.startswith("I-depart_date"):
+            parsed['departureDate'] += ' ' + word
+        elif label.startswith("B-depart_time"):
+            parsed['departureTime'] = word
+        elif label.startswith("I-depart_time"):
+            parsed['departureTime'] += ' ' + word
+        elif label.startswith("B-return_date"):
+            parsed['returnDate'] = word
+        elif label.startswith("I-return_date"):
+            parsed['returnDate'] += ' ' + word
+
+    return parsed
